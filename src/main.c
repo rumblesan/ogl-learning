efe7a8ff7fcb70e6ac4fcc056c093262571a6ea1
Guy John <guy@rumblesan.com>
2015-12-26 15:29:24 +0000

    Example 1-3

--- src/main.c
+++ src/main.c
@@ -2,29 +2,54 @@
 
 #include <GLUT/glut.h>
 
+static GLfloat spin = 0.0;
+
+void init(void) {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
+
 void display(void) {
 
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glPushMatrix();
+    glRotatef(spin, 0.0, 0.0, 1.0);
     glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.25, 0.25, 0.0);
-        glVertex3f(0.75, 0.25, 0.0);
-        glVertex3f(0.75, 0.75, 0.0);
-        glVertex3f(0.25, 0.75, 0.0);
-    glEnd();
-
-    glFlush();
+    glRectf(-25.0, -25.0, 25.0, 25.0);
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
-void init(void) {
+void spinDisplay(void) {
+    spin = spin + 2.0;
+    if (spin > 360.0)
+        spin = spin - 360;
+    glutPostRedisplay();
+}
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-
+void reshape(int w, int h) {
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
 
+void mouse(int button, int state, int x, int y) {
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(spinDisplay);
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(NULL);
+            break;
+        default:
+            break;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -33,9 +58,11 @@ int main(int argc, char* argv[]) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(250, 250);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("hello");
+    glutCreateWindow(argv[0]);
     init();
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
     glutMainLoop();
     return 0;
 }
